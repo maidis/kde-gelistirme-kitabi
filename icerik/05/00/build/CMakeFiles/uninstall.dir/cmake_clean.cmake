871d45d4ae4d3dce5d7166dd5973cb3be74f4681
Anıl Özbek <ozbekanil@gmail.com>
2013-04-20 20:20:12 +0300

    Sadece yeni bir dal acmak icin

---
+++ icerik/05/00/build/CMakeFiles/uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "tutorial1_automoc.cpp"
+  "tutorial1.shell"
+  "CMakeFiles/uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ icerik/05/00/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/maidis/Gelistirme/c++/kde-gelistirme-kitabi/icerik/05/00
+# Build directory: /home/maidis/Gelistirme/c++/kde-gelistirme-kitabi/icerik/05/00/build
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
